5383bd7e930030b5ac07480648ef001e4e8951e5
dennistaiti <dennistaiti7@gmail.com>
2022-12-06 20:43:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  return fetch("https://anapioficeandfire.com/api/books")
+.then((response) => response.json())
+.then((data) => {
+  renderBooks(data)
+  })
 }
 
 function renderBooks(books) {
@@ -14,4 +19,5 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
-});
+});  
+ 
